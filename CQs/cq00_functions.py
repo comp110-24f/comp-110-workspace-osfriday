bcd530d08785ca147355e429f6d11a729b284f23
Olivia Friday <osfriday@ad.unc.edu>
2024-08-29 16:29:53 -0400

    CQ01 first save

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,12 @@
+"""CQ01 in COMP 110"""
+
+__author__ = "730566893"
+
+
+def mimic(message: str) -> str:
+    """Repeat a message back to you."""
+
+
+def main () -> None
+    """print the result of calling mimic function."""
+    print(mimic(message="Howdy"))
---
+++ lessons/functions.py
@@ -0,0 +1,11 @@
+"""Practice with functions"""
+
+
+# Define a function
+def sum(num1: int, num2: int) -> int:
+    """Return num1 + num2."""
+    return num1 + num2
+
+
+# Call a function
+print(sum(num1=1, num2=10))  # <- 1 and 10 are arguments
