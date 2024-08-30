db47b6f8ee7b7b67af70aa6b7ea16c12b4e0e692
Olivia Friday <osfriday@ad.unc.edu>
2024-08-29 20:12:18 -0400

    CQ00 Complete!

--- CQs/cq00_functions.py
+++ CQs/cq00_functions.py
@@ -1,12 +1,17 @@
-"""CQ01 in COMP 110"""
+""" CQ00 Functions COMP 110!"""
 
 __author__ = "730566893"
 
 
 def mimic(message: str) -> str:
-    """Repeat a message back to you."""
+    """repeat input string back to you."""
+    return message
 
 
-def main () -> None
-    """print the result of calling mimic function."""
-    print(mimic(message="Howdy"))
+def main() -> None:
+    """print what returns with mimic function."""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
