8c876f7c28c6c8bf1c6762424762925e6aee1c7f
osfriday <osfriday@ad.unc.edu>
2024-10-06 22:46:41 -0400

    Working on EX04

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,52 @@
+"""Working with Lists for EX04!"""
+
+__author__ = "730566893"
+
+
+def all(a: list[int], n: int) -> bool:
+    """check that each entry in list contains the same value n."""
+    i: int = 0
+    same: bool = True
+    if len(a) == 0:
+        same = False
+    while i < len(a):
+        if a[i] != n:
+            same = False
+        i += 1
+    return same
+
+
+def max(num: list[int]) -> int:
+    """Finds max value in list."""
+    if len(num) == 0:
+        raise ValueError("max() arg is an empty List")
+    idx: int = 1
+    max: int = num[0]
+    while idx < len(num):
+        if max < num[idx]:
+            max = num[idx]  # max is reassigned to the greater value
+        idx += 1
+    return max
+
+
+def is_equal(list_1: list[int], list_2: list[int]) -> bool:
+    """Checks that two lists contain deep equality."""
+    idx: int = 0
+    ans: bool = True
+    if len(list_1) != len(list_2):
+        ans = False
+    while idx < len(list_1):  # what happens if list_1 and list_2 are different lengths?
+        if (
+            list_1[idx] != list_2[idx]
+        ):  # only wants ans to change if there is a non-equal idx
+            ans = False
+        idx += 1
+    return ans
+
+
+def extend(x: list[int], y: list[int]) -> None:
+    """Adds entry from one list to the end of the other."""
+    idx: int = 0
+    while idx < len(y):
+        x.append(y[idx])  # Do not have to worry about overlap
+        idx += 1  # to prevent infinite loop and check each index in y
